289e8ae779de5dd824c0dfaf3f587184ddaab9cf
HenryMarsden <59068823+HenryMarsden@users.noreply.github.com>
2025-08-08 15:51:37 +0100

    Create TestScript.py

---
+++ python/TestScript.py
@@ -0,0 +1,29 @@
+from PCA9685 import PCA9685
+import time
+
+class TestScript:
+  pwm = None
+  
+  def __init__(self):
+    self.pwm = PCA9685()
+    self.pwm.setPWMFreq(50)
+    
+  def tilt(self, rang, incr):
+    self.calibrate()
+    for i in range(6, rang + 2, incr):
+      self.pwm.stutterRotation(0, i)
+      print(self.calcDropOff())
+      time.sleep(60) # Change value for time between rotations, ensure it is a multiple of the Iris data capture time window
+
+  def calibrate(self):
+    self.pwm.calibrate()
+
+  def calcDropOff(self):
+    radius = 2.85
+    distance 9.75
+    return self.pwm.calcInvSqu(distance, radius)
+
+# calibrate between and before every test. Uncomment lines as needed.
+ts = TestScript()
+#ts.tilt(80, 5) 
+ts.calibrate()
