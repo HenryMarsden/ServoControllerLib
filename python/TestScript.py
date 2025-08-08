beb534680bebe71eef6a06296444dfc1f927e5a3
HenryMarsden <59068823+HenryMarsden@users.noreply.github.com>
2025-08-08 15:23:08 +0000

    Update TestScript.py

--- python/TestScript.py
+++ python/TestScript.py
@@ -12,16 +12,11 @@ class TestScript:
     self.calibrate()
     for i in range(6, rang + 2, incr):
       self.pwm.stutterRotation(0, i)
-      print(self.calcDropOff())
       time.sleep(59.5) # Change value for time between rotations, ensure it is a multiple of the Iris data capture time window
 
   def calibrate(self):
     self.pwm.calibrate()
 
-  def calcDropOff(self):
-    radius = 2.85
-    distance = 9.75
-    return self.pwm.calcInvSqu(distance, radius)
 
 # calibrate between and before every test. Uncomment lines as needed.
 ts = TestScript()
